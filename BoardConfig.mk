df6815719e1af0129cb7d6dc9af83bebca2d8ec6
ZaraKnYu <razenxizu@gmail.com>
2026-03-05 10:28:56 +0530

    warm: Add qspa and hypervisor Bootconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,12 +72,15 @@ BOARD_KERNEL_IMAGE_NAME := Image
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
+    disable_dma32=on \
     swinfo.fingerprint=$(LINEAGE_VERSION) \
     mtdoops.fingerprint=$(LINEAGE_VERSION)
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
+    androidboot.hypervisor.protected_vm.supported=0 \
     androidboot.memcg=1 \
+    androidboot.vendor.qspa=true \
     androidboot.usbcontroller=4e00000.dwc3 \
     androidboot.usb.dwc3_msm=4e00000.ssusb \
     androidboot.load_modules_parallel=true
